8092a26b242ce6edc5c094413ba7dcb3e55d4d6b
Yurii <jurabartkiv@gmail.com>
2024-08-13 20:27:27 -0500

    created button event

--- classWork1/main.js
+++ classWork1/main.js
@@ -4,23 +4,34 @@ const button = document.querySelector(".addTaskButton");
 const input = document.querySelector("input");
 const tasksDiv = document.querySelector(".tasksMain");
 userId = 0;
-button.addEventListener("click", () => {
+
+function render() {
   userId++;
   tasks.id = userId;
   tasks.title = input.value;
   tasks.status = "toDo";
 
   const task = `
-    <div class="task">
-  <p>${tasks.id}</p>
-  <p>${tasks.title}</p>
-  <p>${tasks.status}</p>
-  <button class="removeButton"><i class="fa-solid fa-trash"></i></button>
-  </div>`;
+      <div class="task">
+    <p>${tasks.id}</p>
+    <p>${tasks.title}</p>
+    <p>${tasks.status}</p>
+    <button class="removeButton"><i class="fa-solid fa-trash"></i></button>
+    </div>`;
   tasksDiv.insertAdjacentHTML("beforeend", task);
 
   const value = input.value;
   if (value) {
     input.value = "";
+  }
+}
+
+button.addEventListener("click", () => {
+  render();
+});
+
+input.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    render();
   }
 });
