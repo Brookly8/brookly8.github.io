2829d075408fb426d16e8f302e22044a73e4a4ac
Yurii <jurabartkiv@gmail.com>
2024-08-16 20:05:46 -0500

    created movie website v1.0

---
+++ moviesWebSite/main.js
@@ -0,0 +1,55 @@
+import { movies } from "./data.js";
+
+const leftSide = document.querySelector(".leftSide");
+const rightSide = document.querySelector(".rightSide");
+
+const favoriteMovies = [];
+
+function render() {
+  movies.forEach((element) => {
+    const movie = `
+    <div class="movie">
+          <img src="${element.poster_path}"/>
+        <div class="titleMain">
+            <h3 class="title">Title: ${element.original_title}</h3>
+            <h4 class="votes">IMdB: ${element.vote_average}</h4>
+          </div>
+          <p class="discription">${element.overview}</p>
+          <div><button class="addFavorite">Add to Favorite</button></div>
+        </div>
+        
+    `;
+
+    leftSide.insertAdjacentHTML("beforeend", movie);
+  });
+
+  const addFavorite = document.querySelectorAll(".addFavorite");
+
+  addFavorite.forEach((button) => {
+    const btn =
+      button.parentElement.previousElementSibling.previousElementSibling
+        .firstElementChild.textContent;
+
+    button.addEventListener("click", () => {
+      favoriteMovies.push(btn);
+
+      const finalTitle = favoriteMovies[favoriteMovies.length - 1].slice(7);
+      const favorites = `
+      <div class = "FavContainer">
+      <li>${finalTitle}</li>
+      <i class="fa-solid fa-trash removeBtn"></i>
+      </div>`;
+      rightSide.insertAdjacentHTML("beforeend", favorites);
+
+      const removeButtons = document.querySelectorAll(".removeBtn");
+
+      removeButtons.forEach((button) => {
+        button.addEventListener("click", function () {
+          this.parentElement.remove();
+        });
+      });
+    });
+  });
+}
+
+render();
